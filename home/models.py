0a60c349205ab9fd16c2e20abfb683ddfdbaf9a2
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-13 11:42:40 +0330

    start a shopping project and writ model in home app

---
+++ home/models.py
@@ -0,0 +1,34 @@
+from typing import Any
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    is_enable = models.BooleanField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        db_table = 'categorys'
+
+    
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    categories = models.ManyToManyField('Category')
+    is_enable = models.BooleanField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        db_table = 'products'
+
+    
