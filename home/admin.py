100a68283d60b45bb812d6b7a6b2d88fabc8dcff
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-13 12:25:06 +0330

    write admin in home app

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','is_enable','created_time']
+    list_filter = ['is_enable']
+    search_fields = ['title']
+
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','is_enable','created_time']
+    list_filter = ['is_enable']
+    filter_horizontal = ['categories']
+    search_fields = ['title']
