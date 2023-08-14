20a113b7b81dfdcefd31afcbefd3d22e0761efd9
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-14 12:32:05 +0330

    write jwt for product and category and chench urls

--- home/views.py
+++ home/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import Category,Product
 from .serializers import CategorySerializer,ProductSerializer
 
 
 class CategoryViewSetApi(viewsets.ModelViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class ProductViewSetApi(viewsets.ModelViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
