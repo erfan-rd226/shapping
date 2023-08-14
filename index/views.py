96766b8348fe4e76a6d16406baa9ad407ed38d6c
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-14 13:12:29 +0330

    write views index app

--- index/views.py
+++ index/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from home.models import Category,Product
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET'])
+def CategoriesVeiws(request:Request):
+    category = list(Category.objects.all())
+    return Response({'categoy':category}, status.HTTP_200_OK)
+
+
+
+@api_view(['GET'])
+def productsVeiws(request:Request):
+    product = list(Product.objects.all())
+    return Response({'product':product}, status.HTTP_200_OK)
