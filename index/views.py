a9aa12514980ca9a460de51f7009cb2f23ff1299
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-16 15:37:30 +0330

    added filter in indexapp

--- index/views.py
+++ index/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from home.models import Category,Product
 from home.views import paginationshop
-from rest_framework import generics
 from home.serializers import CategorySerializer ,ProductSerializer
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CategoriesGenerics(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = paginationshop
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'is_enable']
 
 class ProductsGenerics(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = paginationshop
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['categories','title'] 
