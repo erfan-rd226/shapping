4a28621913c34c3cbc277c437cf1b86f0648bf3c
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-16 15:18:45 +0330

    chenge urls to generics

--- index/views.py
+++ index/views.py
@@ -8,24 +8,11 @@ from rest_framework import generics
 from home.serializers import CategorySerializer ,ProductSerializer
 
 
-# @api_view(['GET'])
-# def CategoriesVeiws(request:Request):
-#     category = Category.objects.all()
-#     serializer = CategorySerializer(category, many=True)
-#     return Response(serializer.data, status.HTTP_200_OK)
-
-
-# @api_view(['GET'])
-# def productsVeiws(request:Request):
-#     product = Product.objects.all()
-#     serializer = ProductSerializer(product, many=True)
-#     return Response(serializer.data, status.HTTP_200_OK)
-
 class CategoriesGenerics(generics.ListAPIView):
-    category = Category.objects.all()
-    serializer = CategorySerializer(category, many=True)
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
 
 class ProductsGenerics(generics.ListAPIView):
-    product = Product.objects.all()
-    serializer = ProductSerializer(product, many=True) 
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
