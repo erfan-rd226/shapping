0be500c0c822e1198966b25222ff80b7ed56ee72
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-16 15:05:33 +0330

    added generics view in index

--- index/views.py
+++ index/views.py
@@ -4,18 +4,28 @@ from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
 from home.serializers import CategorySerializer ,ProductSerializer
 
 
-@api_view(['GET'])
-def CategoriesVeiws(request:Request):
+# @api_view(['GET'])
+# def CategoriesVeiws(request:Request):
+#     category = Category.objects.all()
+#     serializer = CategorySerializer(category, many=True)
+#     return Response(serializer.data, status.HTTP_200_OK)
+
+
+# @api_view(['GET'])
+# def productsVeiws(request:Request):
+#     product = Product.objects.all()
+#     serializer = ProductSerializer(product, many=True)
+#     return Response(serializer.data, status.HTTP_200_OK)
+
+class CategoriesGenerics(generics.ListAPIView):
     category = Category.objects.all()
     serializer = CategorySerializer(category, many=True)
-    return Response(serializer.data, status.HTTP_200_OK)
 
 
-@api_view(['GET'])
-def productsVeiws(request:Request):
+class ProductsGenerics(generics.ListAPIView):
     product = Product.objects.all()
-    serializer = ProductSerializer(product, many=True)
-    return Response(serializer.data, status.HTTP_200_OK)
+    serializer = ProductSerializer(product, many=True) 
