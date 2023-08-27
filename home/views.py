4b989959e8a8f5dc9b4494ac1d76dc812c4ce9a8
erfan-rd226 <erfan.rajabzade0226@gmail.com>
2023-08-27 11:44:19 +0330

    change view

--- home/views.py
+++ home/views.py
@@ -1,7 +1,41 @@
+# from django.shortcuts import render
+# from django.conf import settings
+# from rest_framework import viewsets
+# from rest_framework import generics
+# from rest_framework.permissions import IsAuthenticated
+# from rest_framework_simplejwt.authentication import JWTAuthentication
+# from rest_framework.pagination import PageNumberPagination
+# from django_filters.rest_framework import DjangoFilterBackend
+# from .models import Category,Product
+# from .serializers import CategorySerializer,ProductSerializer
+
+
+# class paginationshop(PageNumberPagination):
+#     page_size = settings.PAGINATION_PAGE_SIZE
+
+
+# class CategoryViewSetApi(generics.ListAPIView):
+#     permission_classes = [IsAuthenticated]
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+#     pagination_class = paginationshop
+#     filter_backends = [DjangoFilterBackend]
+#     filterset_fields = ['title','is_enable']
+
+
+# class ProductViewSetApi(generics.ListAPIView):
+#     permission_classes = [IsAuthenticated]
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     pagination_class = paginationshop
+#     filter_backends = [DjangoFilterBackend]
+#     filterset_fields = ['categories','title']
+    
+
+
 from django.shortcuts import render
 from django.conf import settings
 from rest_framework import viewsets
-from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.pagination import PageNumberPagination
@@ -14,7 +48,8 @@ class paginationshop(PageNumberPagination):
     page_size = settings.PAGINATION_PAGE_SIZE
 
 
-class CategoryViewSetApi(generics.ListAPIView):
+class CategoryViewSetApi(viewsets.ModelViewSet):
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
@@ -23,11 +58,12 @@ class CategoryViewSetApi(generics.ListAPIView):
     filterset_fields = ['title','is_enable']
 
 
-class ProductViewSetApi(generics.ListAPIView):
+class ProductViewSetApi(viewsets.ModelViewSet):
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     pagination_class = paginationshop
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['categories','title']
+    filterset_fields = ['categories','title']   
     
